8b87e35914820122a1a404e085751472e7ea9446
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-04-02 09:35:21 +0500

    created options for the PhotoShop edit

--- PhotoShop/Pro_16/src/App.jsx
+++ PhotoShop/Pro_16/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
 
   return (
- <h1>ok</h1>
+ <h1></h1>
   )
 }
 
---
+++ PhotoShop/Pro_16/src/components/options/Options.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './options.css'
+
+function Options() {
+  return (
+    <div className='options'>
+<button  className='btn'>Brightness</button>
+<button className='btn'>Contrast</button>
+<button className='btn'>Saturation</button>
+<button className='btn'>Grayscale</button>
+<button className='btn'>Sepla</button>
+<button className='btn'>Hue Rotate</button>
+<button className='btn'>Blur</button>
+      
+    </div>
+  )
+}
+
+export default Options
